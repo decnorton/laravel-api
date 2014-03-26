166e17b2a61dbc503a059be473594a817d0666ef
Dec Norton <decnorton@gmail.com>
2014-03-26 18:19:01 +0000

    Use 403 by default

--- src/Dec/Api/Exceptions/NotAuthorizedException.php
+++ src/Dec/Api/Exceptions/NotAuthorizedException.php
@@ -2,7 +2,7 @@
 
 class NotAuthorizedException extends \Exception {
 
-    public function __construct($message = "Not Authorized", $code = 401, Exception $previous = null)
+    public function __construct($message = "Not authorized", $code = 403, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
